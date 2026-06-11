d5205ead3b0872da538c18424acbc62f2c8def56
Tichita7 <111648697+tichita7@users.noreply.github.com>
2026-06-11 11:21:00 +0530

    Fixed sigin issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,12 @@ const App = () => {
       <Route path="/sign-up" element={<SignUpPage />} />
       <Route
         path="/sso-callback"
-        element={<AuthenticateWithRedirectCallback />}
+        element={
+          <AuthenticateWithRedirectCallback
+            signInForceRedirectUrl="/ai"
+            signUpForceRedirectUrl="/ai"
+          />
+        }
       />
 
       {/* PROTECTED */}
